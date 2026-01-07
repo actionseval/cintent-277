ea118f591988cf591c579c2099207623087a6912
JavidDitty <javidditty@gmail.com>
2026-01-06 22:33:12 -0500

    "Initial commit"

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import, division, print_function
+
+from setuptools import setup
+
+setup(
+    include_package_data=True,
+    setup_requires=['pbr'],
+    pbr=True,
+)
